2aa073205674c3a722b7cc531459c08c7fea3b5f
Matt Johnston <matt@ucc.asn.au>
2023-07-08 19:18:41 +0800

    Refactoring, have air_rate parameter etc

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+# For micropython and an ebyte E32-900T30D lora serial module.
+
 import machine
 from machine import Pin, UART
 import binascii
@@ -78,7 +80,6 @@ class NoLed:
     def off(self):
         pass
 
-
 def get_plat():
     # m5stack specific
     m5_board_name = None
@@ -101,24 +102,115 @@ def get_plat():
     # TODO
     return MattPico()
 
-def demo(baud = 9600):
-    plat = get_plat()
-    u = UART(plat.uart, baud, tx=plat.tx, rx=plat.rx)
-    m0 = Pin(plat.m0, Pin.OUT)
-    m1 = Pin(plat.m1, Pin.OUT)
-    return u, m0, m1
-
-
 def hex(s):
     if s:
-        return binascii.hexlify(s)
+        return binascii.hexlify(s).decode()
     return s
 
-def read_timeout(uart, timeout):
-    poll = select.poll()
-    poll.register(uart, select.POLLIN)
-    if poll.poll(timeout):
-        return uart.read()
+class Lora:
+    def __init__(self, plat, freq, air_rate):
+        self.plat = plat
+        self.freq = freq
+        self.air_rate = air_rate
+        self.uart = UART(plat.uart, 9600, tx=plat.tx, rx=plat.rx)
+        try:
+            self.led = NeoPixel(plat.neopixel)
+        except AttributeError:
+            try:
+                print(plat.led)
+                self.led = Led(plat.led)
+            except AttributeError:
+                self.led = NoLed()
+
+        self.setup()
+
+    def read_timeout(self, timeout = 2000):
+        poll = select.poll()
+        poll.register(self.uart, select.POLLIN)
+        if poll.poll(timeout):
+            return self.uart.read()
+
+    def write(self, b):
+        self.uart.write(b)
+
+    def setup(self):
+        BASE_FREQ = 862
+        UART_8N1_9600 = 0x18
+        RATES = {
+            300: 0,
+            1200: 1,
+            2400: 2,
+            4800: 3,
+            9600: 4,
+            19200: 5,
+        }
+
+        OPT_FEC = 1<<2
+        OPT_POWER_30 = 0
+        OPT_POWER_27 = 1
+        OPT_POWER_24 = 2
+        OPT_POWER_21 = 3
+        OPT_PUSHPULL = 1 << 6
+        OPT_FIXED_TRANS = 1 << 7
+
+        m0 = Pin(self.plat.m0, Pin.OUT)
+        m1 = Pin(self.plat.m1, Pin.OUT)
+        m0.value(0)
+        m1.value(0)
+
+        #enter configuration mode
+        m0.value(1)
+        m1.value(1)
+        self.led.on()
+
+        time.sleep(0.05)
+
+        # version info
+        code1 = bytes([0xC3,0xC3,0xC3])
+        l = self.uart.write(code1)
+        time.sleep(0.05)
+        z=self.uart.read()
+        print("version {}".format(hex(z)))
+
+        # old config info
+        code1 = bytes([0xC1,0xC1,0xC1])
+        l = self.uart.write(code1)
+        time.sleep(0.05)
+        z=self.uart.read()
+        print("old config {}".format(hex(z)))
+
+        # Australia allows 915-928 at 1W
+        assert 915 <= self.freq <= 928
+        chan = self.freq - 862
+        addr = 0xffff
+        sped = UART_8N1_9600 | RATES[self.air_rate]
+        # 30dBm is 1W, the limit. we have a 6dB antenna
+        opt = OPT_PUSHPULL | OPT_FEC | OPT_POWER_24
+        code3=bytes([0xc2, addr >> 8, addr & 0xff, sped, chan, opt])
+        print("config {}".format(hex(code3)))
+        self.uart.write(code3)
+        time.sleep(0.05)
+        z=self.uart.read()
+        print("config returned {}".format(hex(z)))
+
+        # new config info
+        code1 = bytes([0xC1,0xC1,0xC1])
+        l = self.uart.write(code1)
+        time.sleep(0.05)
+        z=self.uart.read()
+        print("new config {}".format(hex(z)))
+
+        #enter operation mode
+        m0.value(0)
+        m1.value(0)
+        self.led.off()
+        time.sleep(0.05)
+
+def try_decode(b):
+    try:
+        return b.decode()
+    except UnicodeDecodeError:
+        return str(b)
 
 def run():
     print("run")
@@ -126,88 +218,28 @@ def run():
     plat = get_plat()
     print("plat {}".format(plat))
 
-    uart = UART(plat.uart, 9600, tx=plat.tx, rx=plat.rx)
-    print("uart")
+    lora = Lora(plat, 920, 300)
 
-    try:
-        led = NeoPixel(plat.neopixel)
-    except AttributeError:
-        try:
-            print(plat.led)
-            led = Led(plat.led)
-        except AttributeError:
-            led = NoLed()
-
-    print("ledon")
-    led.on()
-    time.sleep(1)
-    led.off()
-    print("ledoff")
-
-    time.sleep(0.2)
-
-    m0 = Pin(plat.m0, Pin.OUT)
-    m1 = Pin(plat.m1, Pin.OUT)
-    m0.value(0)
-    m1.value(0)
-
-    led.on()
-
-    #enter configuration mode
-
-    m0.value(1)
-    m1.value(1)
-
-    time.sleep(1)
-
-    code1 = bytes([0xC3,0xC3,0xC3])
-    l = uart.write(code1)
-    time.sleep(0.05)
-    print("wrote {}".format(l))
-    z=uart.read()
-
-    print(hex(z))
-
-    #write operation config.
-    #key is 0xAAAA
-
-    # Australia allows 915-928 at 1W
-    freq = 920
-    chan = freq - 862
-    assert 915 <= freq <= 928
-
-    code3=bytes([0xc2,0xAA,0xAA,0x1a,chan,0x44])
-    uart.write(code3)
-    time.sleep(0.05)
-    z=uart.read()
-
-    print(z)
-    print("config")
-    print(hex(z))
-
-    #enter operation mode
-    m0.value(0)
-    m1.value(0)
-    led.off()
-    time.sleep(0.05)
+    print("Running")
 
     message_counter = 0
-
-    print("Hello World!")
-
     while True:
-        
+        pending_newline = False
         message_counter +=1
-        z=read_timeout(uart, 2000)
+        z = lora.read_timeout()
         if z:
-            led.on()
+            lora.led.on()
             msg = "twoway {}".format(message_counter)
-            uart.write(msg)
-            print("read: {}".format(z))
+            lora.write(msg)
+            if pending_newline:
+                print()
+                pending_newline = False
+            print("read: {}".format(try_decode(z)))
             print("message counter", message_counter)
         else:
-            led.off()
+            lora.led.off()
             print(".", end="")
+            pending_newline = True
             msg = "radio {}".format(message_counter)
-            uart.write(msg)
+            lora.write(msg)
 
